a729914e3190742a374f1cc0e49235413a1e6487
Kancy Joe <sunflower2333@outlook.com>
2024-02-06 18:16:58 +0800

    Patcher: Add support for different format of kernel. - Fix bug while patching kernel with qcom patched kenrel magic("UNCOMPRESSED_IMG" + 4 bytes kernel_size). - Add support for kernel with efi stub.

--- patcher.c
+++ patcher.c
@@ -1,3 +1,17 @@
+/** @file
+ *  DualBoot Kernel Patcher Source File.
+ *
+ *  This Program will help you inject shell code into header of linux kernel.
+ *
+ *  It only supports several formats of kernel:
+ *      1. Image file compile from source
+ *      2. Qualcomm patched kernel.
+ *      3. Image file kernel with efi stub.
+ *
+ *  Copyright (c) 2021-2024 The DuoWoa authors. All rights reserved.
+ *  MIT License
+ *
+ */
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
@@ -5,9 +19,19 @@
 #include <errno.h>
 #include "patcher.h"
 
+/**
+ * The main function will check and read given files,
+ * pass it to patch function, and write patched kernel to file.
+ *
+ * @param argc  argc is numbers of argues given in cmdline.
+ * @param argv  argv is a array that contains all given values.
+ *
+ * @retval  -EINVAL Given files are not found or format not match.
+ *
+ */
 int main(int argc, char *argv[]) {
-    // Print hello message
-    printf("WOA-msmnile DualBoot Kernel Image Patcher v1.0.0.0 !\n");
+    // Print hello message.
+    printf("WOA-msmnile DualBoot Kernel Image Patcher v1.1.0.0\n");
     printf("Copyright (c) 2021-2024 The DuoWoA authors\n\n");
     if (argc != 6) {
         // Print usage if arg numbers not meet.
@@ -16,100 +40,137 @@ int main(int argc, char *argv[]) {
         return -EINVAL;
     }
 
-    // Get file paths
-    char *originImage = argv[1];
-    char *uefiImage = argv[2];
-    char *outputImage = argv[3];
-    char *config = argv[4];
-    char *shellCode = argv[5];
+    // Get file paths.
+    FileContent originImage = {.filePath = argv[1]};
+    FileContent uefiImage = {.filePath = argv[2]};
+    FileContent outputImage = {.filePath = argv[3]};
+    FileContent config = {.filePath = argv[4]};
+    FileContent shellCode = {.filePath = argv[5]};
 
-    printf("Patching %s with %s and saving to %s...\n\n", originImage, uefiImage, outputImage);
+    // Print processing msg.
+    printf("Patching %s with %s and saving to %s...\n\n", originImage.filePath,
+           uefiImage.filePath,
+           outputImage.filePath);
 
     // Read buffer from old kernel.
-    size_t originImageSize = get_file_size(originImage);
-    if (originImageSize == 0) {
-        printf("Error: Please check the kernel you provided.\n");
+    if (!get_file_size(&originImage))
         return -EINVAL;
-    }
-    void *originImageBuffer = malloc(originImageSize);
-    FILE *pOriginImageFile = fopen(originImage, "rb");
-    fread(originImageBuffer, originImageSize, 1, pOriginImageFile);
-    fclose(pOriginImageFile);
+    originImage.fileBuffer = malloc(originImage.fileSize);
+    read_file_content(&originImage);
 
     // Read buffer from uefi image.
-    size_t uefiImageSize = get_file_size(uefiImage);
-    if (uefiImageSize == 0) {
-        printf("Error: Please check the uefi fd you provided.\n");
+    if (!get_file_size(&uefiImage))
         return -EINVAL;
-    }
-    void *uefiImageBuffer = malloc(uefiImageSize);
-    FILE *pUefiImageFile = fopen(uefiImage, "rb");
-    fread(uefiImageBuffer, uefiImageSize, 1, pUefiImageFile);
-    fclose(pUefiImageFile);
+    uefiImage.fileBuffer = malloc(uefiImage.fileSize);
+    read_file_content(&uefiImage);
 
     // Parse config file.
     Stack stack = {0};
-    if (parse_config(config, &stack)) {
-        printf("Error: Please check config your provided.\n");
+    if (parse_config(&config, &stack)) {
+        printf("Error: Parse config failed\n");
         return -EINVAL;
     }
 
     // Get ShellCode buffer.
-    size_t shellCodeSize = get_file_size(shellCode);
-    void *shellCodeBuffer = malloc(shellCodeSize);
-    FILE *pShellCodeFile = fopen(shellCode, "rb");
-    if (pShellCodeFile == NULL) {
-        printf("Error: Please check the shell code binary you provided.\n");
+    if (!get_file_size(&shellCode))
         return -EINVAL;
-    }
-    fread(shellCodeBuffer, shellCodeSize, 1, pShellCodeFile);
-    fclose(pShellCodeFile);
+    shellCode.fileBuffer = malloc(shellCode.fileSize);
+    read_file_content(&shellCode);
 
     // Patch Kernel.
-    size_t outputImageSize = originImageSize + uefiImageSize;
-    uint8_t *outputImageBuffer = PatchKernel(originImageBuffer, originImageSize, uefiImageBuffer, uefiImageSize,
-                                             shellCodeBuffer, shellCodeSize, &stack);
+    PatchKernel(&originImage, &uefiImage, &shellCode, &outputImage, &stack);
 
     // Output buffer to new kernel.
-    if (outputImageBuffer != NULL) {
-        FILE *pOutputImageFile = fopen(outputImage, "wb");
-        fwrite(outputImageBuffer, outputImageSize, 1, pOutputImageFile);
-        fclose(pOriginImageFile);
+    if (outputImage.fileBuffer != NULL) {
+        write_file_content(&outputImage);
     } else {
         printf("Error Patching Kernel.\n");
         return -EINVAL;
     }
 
-    // Free buffers.
-    free(shellCodeBuffer);
-    free(originImageBuffer);
-    free(uefiImageBuffer);
-    free(outputImageBuffer);
+    // Free buffers we allocated.
+    free(shellCode.fileBuffer);
+    free(originImage.fileBuffer);
+    free(uefiImage.fileBuffer);
+    free(outputImage.fileBuffer);
 
     // Print end message.
     printf("Image successfully patched.\n");
-    printf("Please find the newly made kernel image at %s\n", outputImage);
+    printf("Please find the newly made kernel image at %s.\n", outputImage.filePath);
     return 0;
 }
 
-size_t get_file_size(char *filePath) {
-    FILE *pFile = fopen(filePath, "r");
-    if (pFile == NULL)
+/**
+ * Get file size based on given fileContent.
+ *
+ * @param fileContent provide filePath, will also set fileSize in it.
+ * @retval 0    File does not exist.
+ * @retval size_t   File size
+ *
+ */
+size_t get_file_size(FileContent *fileContent) {
+    FILE *pFile = fopen(fileContent->filePath, "r");
+    if (pFile == NULL) {
+        printf("Error: %s not found\n", fileContent->filePath);
         return 0;
+    }
     fseek(pFile, 0, SEEK_END);
     size_t len = ftell(pFile);
     fclose(pFile);
+    fileContent->fileSize = len;
     return len;
 }
 
-int parse_config(char *filePath, pStack stack) {
-    FILE *pConfigFile = fopen(filePath, "r");
+/**
+ * Read File buffer based on given fileContent.
+ *
+ * @param fileContent   provide filePath, will also set fileBuffer in it.
+ * @return  Buffer read from file.
+ */
+uint8_t *read_file_content(FileContent *fileContent) {
+    if (fileContent->fileBuffer == NULL)
+        return NULL;
+    FILE *pFile = fopen(fileContent->filePath, "rb");
+    if (pFile == NULL)
+        return NULL;
+    fread(fileContent->fileBuffer, fileContent->fileSize, 1, pFile);
+    fclose(pFile);
+    return fileContent->fileBuffer;
+}
+
+/**
+ * Write buffer to filePath given by fileContent.
+ *
+ * @param fileContent   Contains file information.
+ * @retval -EBADF   Failed to write file
+ *
+ */
+int write_file_content(pFileContent fileContent) {
+    FILE *pFile = fopen(fileContent->filePath, "wb");
+    if (pFile == NULL)
+        return -EBADF;
+    fwrite(fileContent->fileBuffer, fileContent->fileSize, 1, pFile);
+    fclose(pFile);
+    return 0;
+}
+
+/**
+ * Parse given config.
+ *
+ * @param fileContent
+ * @param stack Stack info read from config file
+ * @retval -EINVAL Give File not found.
+ *
+ */
+int parse_config(FileContent *fileContent, pStack stack) {
+    // Check file size
+    if (!get_file_size(fileContent))
+        return -EINVAL;
+
+    // Open config file
+    FILE *pConfigFile = fopen(fileContent->filePath, "r");
     char key[256];
     uint32_t value = 0;
-
-    // Check File Size
-    if (!get_file_size(filePath))
-        return -1;
 
     // Parse
     while (fscanf(pConfigFile, "%[^=]=%x\n", key, &value) != EOF) {
@@ -124,30 +185,76 @@ int parse_config(char *filePath, pStack stack) {
     return 0;
 }
 
-uint8_t *PatchKernel(uint8_t *kernelBuffer, size_t kernelBufferSize, uint8_t *uefiBuffer, size_t uefiBufferSize,
-                     uint8_t *shellCodeBuffer, size_t shellCodeSize, pStack stack) {
 
-    // Check if kernel has UNCOMPRESSED_IMG Header.
-    char kernelHeader[0x11] = {0};
-    memcpy(kernelHeader, kernelBuffer, 0x10);
-    if (strcmp(kernelHeader, "UNCOMPRESSED_IMG") == 0) {
-        printf("Kernel has header, removing it...\n");
-        kernelBuffer += 0x14;
-        kernelBufferSize -= 0x14;
-    }
-
-    // Allocate patched image buffer.
-    size_t patchedKernelBufferSize = kernelBufferSize + uefiBufferSize;
-    uint8_t *patchedKernelBuffer = malloc(patchedKernelBufferSize);
+/**
+ * Patch kernel based on given fileContents,
+ *
+ * @param[in] kernel    origin kernel fileContent
+ * @param[in] uefi  uefi fd fileContent
+ * @param[in] shellCode shell code binary
+ * @param[in,out] patchedKernel patched kernel fileContent
+ * @param[in] stack stack info read from config
+ *
+ * @return patched kernel buffer
+ *
+ */
+uint8_t *PatchKernel(pFileContent kernel, pFileContent uefi, pFileContent shellCode,
+                     pFileContent patchedKernel, pStack stack) {
+    // Allocate output buffer
+    patchedKernel->fileSize = kernel->fileSize + uefi->fileSize;
+    patchedKernel->fileBuffer = malloc(patchedKernel->fileSize);
 
     // Copy two buffers into patchedBuffer.
-    memcpy(patchedKernelBuffer, kernelBuffer, kernelBufferSize);
-    memcpy(patchedKernelBuffer + kernelBufferSize, uefiBuffer, uefiBufferSize);
+    memcpy(patchedKernel->fileBuffer, kernel->fileBuffer, kernel->fileSize);
+    memcpy(patchedKernel->fileBuffer + kernel->fileSize, uefi->fileBuffer, uefi->fileSize);
 
-    if (uefiBufferSize > 0x40 && strcmp((char *) (shellCodeBuffer + 8), "SHLLCOD") != 0) {
+    // Check ShellCode binary magic.
+    if (uefi->fileSize > 0x40 && strcmp((char *) (shellCode->fileBuffer + 8), "SHLLCOD") != 0) {
         printf("Error: shell code binary format not recognize.\n");
         return NULL;
     }
+
+    // Check if kernel has UNCOMPRESSED_IMG Header.
+    char kernelHeader[0x11] = {0};
+    uint8_t hasHeader = 0;
+    memcpy(kernelHeader, kernel->fileBuffer, 0x10);
+    if (strcmp(kernelHeader, "UNCOMPRESSED_IMG") == 0) {
+        printf("Kernel has UNCOMPRESSED_IMG header.\n");
+        kernel->fileSize -= 0x14;   // The magic is not part of kernel.
+        hasHeader |= 0b1;
+    }
+
+    // Check if kernel size % 0x10 == 0, which will cause copy loop issue.
+    if (kernel->fileSize % 0x10 != 0) {
+        printf("Align kernel size to 0x10.\n");
+        // Reallocate patched kernel.
+        void *ptr = realloc(patchedKernel->fileBuffer, patchedKernel->fileSize + (kernel->fileSize % 10));
+        if (ptr == NULL) {
+            printf("Failed to reallocate patched kernel buffer.");
+            return NULL;
+        } else patchedKernel->fileBuffer = ptr;
+        // Copy uefi image to 0x10 align place.
+        uint8_t padding = 0x10 - (kernel->fileSize % 0x10);
+        memmove(patchedKernel->fileBuffer + kernel->fileSize + padding,
+                patchedKernel->fileBuffer + kernel->fileSize, uefi->fileSize);
+        kernel->fileSize += padding;
+        patchedKernel->fileSize += padding;
+    }
+
+    // Check if kernel has EFI Stub, 4D5A0091, "MZ"
+    // Note: This magic is part of kernel
+    // We wil rewrite file header here.
+    // For kernel with EFI stub, we only need to patch the efi header.
+    memcpy(kernelHeader, kernel->fileBuffer, 0x4);
+    if (*(uint32_t *) kernelHeader == 0x91005A4D) {
+        printf("Kernel has EFI header.\n");
+        hasHeader |= 0b10;
+    }
+
+    // Kernel has uncompressed_img header,
+    if(hasHeader & 1)
+        // Move our pointer after UNCOMPRESSED_IMG header before further processing,
+        patchedKernel->fileBuffer += 0x14;
 
     // Determine the loading offset of the kernel first,
     // we are either going to find a b instruction on the
@@ -155,11 +262,13 @@ uint8_t *PatchKernel(uint8_t *kernelBuffer, size_t kernelBufferSize, uint8_t *ue
     // second is fine.
 
     // 0x14 is AArch64 b opcode
-    if (patchedKernelBuffer[3] == 0x14) {
+    if (patchedKernel->fileBuffer[3] == 0x14) {
+        // For kernel without EFI stub, we have to move the jump kernel instruction forward to Code2.
         // We have a branch instruction first, we need to fix things a bit.
 
         // First start by getting the actual value of the branch addr offset.
-        uint8_t offsetInstructionBuffer[] = {patchedKernelBuffer[0], patchedKernelBuffer[1], patchedKernelBuffer[2], 0};
+        uint8_t offsetInstructionBuffer[] = {patchedKernel->fileBuffer[0], patchedKernel->fileBuffer[1],
+                                             patchedKernel->fileBuffer[2], 0};
         uint32_t *offsetInstruction = (uint32_t *) offsetInstructionBuffer;
 
         // Now subtract 1 instruction because we'll move this to the second instruction of the kernel header.
@@ -167,17 +276,16 @@ uint8_t *PatchKernel(uint8_t *kernelBuffer, size_t kernelBufferSize, uint8_t *ue
 
         // Convert back into an actual value that is usable as a b instruction
         offsetInstructionBuffer[3] = 0x14; // Useless but just for our sanity :)
-        // Now write the instruction back into the kernel (instr 2)
-        patchedKernelBuffer[4] = offsetInstructionBuffer[0];
-        patchedKernelBuffer[5] = offsetInstructionBuffer[1];
-        patchedKernelBuffer[6] = offsetInstructionBuffer[2];
-        patchedKernelBuffer[7] = 0x14;
 
-    } else if (patchedKernelBuffer[7] != 0x14) {
+        // Now write the instruction back into the kernel (instr 2)
+        patchedKernel->fileBuffer[4] = offsetInstructionBuffer[0];
+        patchedKernel->fileBuffer[5] = offsetInstructionBuffer[1];
+        patchedKernel->fileBuffer[6] = offsetInstructionBuffer[2];
+        patchedKernel->fileBuffer[7] = 0x14;
+    }
+    else if (patchedKernel->fileBuffer[7] != 0x14) {
         // There is no branch instruction!
         printf("Error: Invalid Kernel Image. Branch instruction not found within first two instruction slots.\n");
-        return NULL;
-    } else {
         return NULL;
     }
 
@@ -185,49 +293,60 @@ uint8_t *PatchKernel(uint8_t *kernelBuffer, size_t kernelBufferSize, uint8_t *ue
 
     // First, add the jump to our code on instr 1
     // This directly jump right after the kernel header (there's enough headroom here)
-    patchedKernelBuffer[0] = 0x10;
-    patchedKernelBuffer[1] = 0;
-    patchedKernelBuffer[2] = 0;
-    patchedKernelBuffer[3] = 0x14;
+    patchedKernel->fileBuffer[0] = 0x10;
+    patchedKernel->fileBuffer[1] = 0;
+    patchedKernel->fileBuffer[2] = 0;
+    patchedKernel->fileBuffer[3] = 0x14;
 
     // Now we need to fill in the stack base of our firmware
     // Stack Base: 0x00000000 9FC00000 (64 bit!)
-    patchedKernelBuffer[0x20] = stack->StackBase >> 0 & 0xFF;
-    patchedKernelBuffer[0x21] = stack->StackBase >> 8 & 0xFF;
-    patchedKernelBuffer[0x22] = stack->StackBase >> 16 & 0xFF;
-    patchedKernelBuffer[0x23] = stack->StackBase >> 24 & 0xFF;
-    patchedKernelBuffer[0x24] = stack->StackBase >> 32 & 0xFF;
-    patchedKernelBuffer[0x25] = stack->StackBase >> 40 & 0xFF;
-    patchedKernelBuffer[0x26] = stack->StackBase >> 48 & 0xFF;
-    patchedKernelBuffer[0x27] = stack->StackBase >> 56 & 0xFF;
+    patchedKernel->fileBuffer[0x20] = stack->StackBase >> 0 & 0xFF;
+    patchedKernel->fileBuffer[0x21] = stack->StackBase >> 8 & 0xFF;
+    patchedKernel->fileBuffer[0x22] = stack->StackBase >> 16 & 0xFF;
+    patchedKernel->fileBuffer[0x23] = stack->StackBase >> 24 & 0xFF;
+    patchedKernel->fileBuffer[0x24] = stack->StackBase >> 32 & 0xFF;
+    patchedKernel->fileBuffer[0x25] = stack->StackBase >> 40 & 0xFF;
+    patchedKernel->fileBuffer[0x26] = stack->StackBase >> 48 & 0xFF;
+    patchedKernel->fileBuffer[0x27] = stack->StackBase >> 56 & 0xFF;
 
     // Then we need to fill in the stack size of our firmware
     // Stack Base: 0x00000000 00300000 (64 bit!)
-    patchedKernelBuffer[0x28] = stack->StackSize >> 0 & 0xFF;
-    patchedKernelBuffer[0x29] = stack->StackSize >> 8 & 0xFF;
-    patchedKernelBuffer[0x2A] = stack->StackSize >> 16 & 0xFF;
-    patchedKernelBuffer[0x2B] = stack->StackSize >> 24 & 0xFF;
-    patchedKernelBuffer[0x2C] = stack->StackSize >> 32 & 0xFF;
-    patchedKernelBuffer[0x2D] = stack->StackSize >> 40 & 0xFF;
-    patchedKernelBuffer[0x2E] = stack->StackSize >> 48 & 0xFF;
-    patchedKernelBuffer[0x2F] = stack->StackSize >> 56 & 0xFF;
+    patchedKernel->fileBuffer[0x28] = stack->StackSize >> 0 & 0xFF;
+    patchedKernel->fileBuffer[0x29] = stack->StackSize >> 8 & 0xFF;
+    patchedKernel->fileBuffer[0x2A] = stack->StackSize >> 16 & 0xFF;
+    patchedKernel->fileBuffer[0x2B] = stack->StackSize >> 24 & 0xFF;
+    patchedKernel->fileBuffer[0x2C] = stack->StackSize >> 32 & 0xFF;
+    patchedKernel->fileBuffer[0x2D] = stack->StackSize >> 40 & 0xFF;
+    patchedKernel->fileBuffer[0x2E] = stack->StackSize >> 48 & 0xFF;
+    patchedKernel->fileBuffer[0x2F] = stack->StackSize >> 56 & 0xFF;
 
     // Finally, we add in the total kernel image size because we need to jump over!
-    patchedKernelBuffer[0x30] = kernelBufferSize >> 0 & 0xFF;
-    patchedKernelBuffer[0x31] = kernelBufferSize >> 8 & 0xFF;
-    patchedKernelBuffer[0x32] = kernelBufferSize >> 16 & 0xFF;
-    patchedKernelBuffer[0x33] = kernelBufferSize >> 24 & 0xFF;
-    patchedKernelBuffer[0x34] = kernelBufferSize >> 32 & 0xFF;
-    patchedKernelBuffer[0x35] = kernelBufferSize >> 40 & 0xFF;
-    patchedKernelBuffer[0x36] = kernelBufferSize >> 48 & 0xFF;
-    patchedKernelBuffer[0x37] = kernelBufferSize >> 56 & 0xFF;
+    patchedKernel->fileBuffer[0x30] = kernel->fileSize >> 0 & 0xFF;
+    patchedKernel->fileBuffer[0x31] = kernel->fileSize >> 8 & 0xFF;
+    patchedKernel->fileBuffer[0x32] = kernel->fileSize >> 16 & 0xFF;
+    patchedKernel->fileBuffer[0x33] = kernel->fileSize >> 24 & 0xFF;
+    patchedKernel->fileBuffer[0x34] = kernel->fileSize >> 32 & 0xFF;
+    patchedKernel->fileBuffer[0x35] = kernel->fileSize >> 40 & 0xFF;
+    patchedKernel->fileBuffer[0x36] = kernel->fileSize >> 48 & 0xFF;
+    patchedKernel->fileBuffer[0x37] = kernel->fileSize >> 56 & 0xFF;
 
     // Now our header is fully patched, let's add a tiny bit
     // of code as well to decide what to do.
-    // Ignore 0x40 Dummy Header in shellCodeBuffer, so we add 0x40 offset after it.
-    memcpy(patchedKernelBuffer + 0x40, shellCodeBuffer + 0x40, shellCodeSize - 0x40);
+    // Ignore 0x40 Dummy Header in shellCode->fileBuffer, so we add 0x40 offset after it.
+    memcpy(patchedKernel->fileBuffer + 0x40, shellCode->fileBuffer + 0x40, shellCode->fileSize - 0x40);
+
+    // Move back our pointer and recalculate kernel size in kernel
+    // header if the patched kernel buffer has UNCOMPRESSED_IMG header.
+    if (hasHeader & 0b1) {
+        patchedKernel->fileBuffer -= 0x14;
+        size_t newKernelSize = kernel->fileSize + uefi->fileSize;
+        patchedKernel->fileBuffer[0x10] = newKernelSize >> 0 & 0xFF;
+        patchedKernel->fileBuffer[0x11] = newKernelSize >> 8 & 0xFF;
+        patchedKernel->fileBuffer[0x12] = newKernelSize >> 16 & 0xFF;
+        patchedKernel->fileBuffer[0x13] = newKernelSize >> 24 & 0xFF;
+    }
 
     // And that's it, the user now can append executable code right after the kernel,
     // and upon closing up the device said code will run at boot. Have fun!
-    return patchedKernelBuffer;
+    return patchedKernel->fileBuffer;
 }
--- patcher.h
+++ patcher.h
@@ -1,20 +1,42 @@
-//
-//  Kernel Patcher Header File.
-//
+/** @file
+ * DualBoot Kernel Patcher Header File.
+ *
+ *  Copyright (c) 2021-2024 The DuoWoa authors. All rights reserved.
+ *  MIT License
+ *
+ */
 
 #ifndef LINUX_KERNEL_PATCHER_PATCHER_H
 #define LINUX_KERNEL_PATCHER_PATCHER_H
 
 #include <stdint.h>
 
+//
+//  Stack info read from config file and set to kernel.
+//
 typedef struct {
     uint64_t StackBase;
     uint64_t StackSize;
 } Stack, *pStack;
 
-size_t get_file_size(char *filePath);
-int parse_config(char* filePath, pStack stack);
-uint8_t *PatchKernel(uint8_t *kernelBuffer, size_t kernelBufferSize, uint8_t *uefiBuffer, size_t uefiBufferSize,
-                     uint8_t *shellCodeBuffer, size_t shellCodeSize, pStack stack);
+//
+// Store some file information and file buffer.
+//
+typedef struct {
+    uint8_t *fileBuffer;
+    size_t fileSize;
+    const char *filePath;
+} FileContent, *pFileContent;
+
+size_t get_file_size(FileContent *fileContent);
+
+uint8_t *read_file_content(FileContent *fileContent);
+
+int write_file_content(pFileContent fileContent);
+
+int parse_config(FileContent *fileContent, pStack stack);
+
+uint8_t *PatchKernel(pFileContent kernel, pFileContent uefi, pFileContent shellCode,
+                     pFileContent patchedKernel, pStack stack);
 
 #endif //LINUX_KERNEL_PATCHER_PATCHER_H
